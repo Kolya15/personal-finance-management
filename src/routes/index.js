5c53f67bf0284450a92d3fe250e21bffd5e4d633
Sornik <sornik@goalem.solutions>
2021-09-24 15:00:48 +0300

    + added crud income and expense + global validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,8 @@ router.use('*', varify)
 router.use('/users', require('./api/userRouter'));
 router.use('/auth', require('./api/authRouter'));
 router.use('/income', require('./api/incomeRouter'));
+router.use('/categories/income', require('./api/incomeCategoriesRouter'));
+router.use('/expense', require('./api/expenseRouter'));
+router.use('/categories/expense', require('./api/expenseCategoriesRouter'));
 
 module.exports = router;
